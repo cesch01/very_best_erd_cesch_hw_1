4b40d3055561fadc4c865b16d36c15a93e7d5306
Raghu Betina <raghu@firstdraft.com>
2018-04-05 23:38:17 +0000

    Direct association between venue_addresses and venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  belongs_to :address,
+             :class_name => "VenueAddress"
+
   has_many   :bookmarked_dishes,
              :class_name => "Bookmark",
              :dependent => :destroy
--- app/models/venue_address.rb
+++ app/models/venue_address.rb
@@ -1,6 +1,10 @@
 class VenueAddress < ApplicationRecord
   # Direct associations
 
+  has_one    :venue,
+             :foreign_key => "address_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
